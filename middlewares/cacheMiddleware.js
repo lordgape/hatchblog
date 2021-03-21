011d2235180d0ad3f38f2ee2bec84f0a49a91f44
Chesvic Lordgape <lordgape@gmail.com>
2021-03-21 03:41:01 +0100

    Change caching key

--- middlewares/cacheMiddleware.js
+++ middlewares/cacheMiddleware.js
@@ -2,9 +2,10 @@ const cache = require('memory-cache');
 
 // configure cache middleware
 const memCache = new cache.Cache();
+
 const cacheMiddleware = (duration) => {
   return (req, res, next) => {
-    let key = '__express__' + req.query || {};
+    let key = '__express__' + req.query.tag + req.query.sortBy + req.query.direction || {};
     let cacheContent = memCache.get(key);
     if (cacheContent) {
       res.json(cacheContent);
